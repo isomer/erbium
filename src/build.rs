11237f8405428d823f52f402712a329da0ea06fa
Perry Lorier <git@isomer.meta.net.nz>
2021-03-07 13:47:15 +0000

    Don't collect git information if it fails.

--- src/build.rs
+++ src/build.rs
@@ -1,5 +1,20 @@
 extern crate vergen;
 
 fn main() {
-    vergen::vergen(vergen::Config::default()).expect("Unable to generate the cargo keys!");
+    let mut conf = vergen::Config::default();
+    match vergen::vergen(conf) {
+        Ok(()) => {}
+        Err(vergen::Error::Git2(_)) => {
+            /* If this is not from a git repository, ignore trying to get the git info */
+            *conf.git_mut().branch_mut() = false;
+            *conf.git_mut().commit_timestamp_mut() = false;
+            *conf.git_mut().rerun_on_head_change_mut() = false;
+            *conf.git_mut().semver_mut() = false;
+            *conf.git_mut().sha_mut() = false;
+            vergen::vergen(conf).expect("Unable to get vergen build information");
+        }
+        Err(e) => {
+            panic!("Unable to get vergen build information: {}", e);
+        }
+    }
 }
