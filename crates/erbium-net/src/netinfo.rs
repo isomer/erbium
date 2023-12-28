isomer/erbium

commit 15bde7f729d9dbce1a2bd0070ca219958aafd5db

Bump netlink-sys dependency.

crates/erbium-net/src/netinfo.rs
@@ -25,7 +25,7 @@ use netlink_packet_route::{
     RouteMessage, RtnlMessage,
 };
 use netlink_sys::TokioSocket as Socket;
-use netlink_sys::{protocols, SocketAddr};
+use netlink_sys::{protocols, AsyncSocket as _, AsyncSocketExt as _, SocketAddr};
 
 #[derive(Clone, PartialEq, Eq)]
 pub enum LinkLayer {
@@ -332,7 +332,7 @@ impl NetLinkNetInfo {
 
         packet.serialize(&mut buf[..]);
 
-        socket.add_membership(RTNLGRP_LINK).unwrap();
+        socket.socket_mut().add_membership(RTNLGRP_LINK).unwrap();
 
         if let Err(e) = socket.send(&buf[..]).await {
             log::warn!("SEND ERROR {}", e);
@@ -370,8 +370,14 @@ impl NetLinkNetInfo {
         packet.serialize(&mut buf[..]);
 
         match address_family as u16 {
-            AF_INET => socket.add_membership(RTNLGRP_IPV4_ROUTE).unwrap(),
-            AF_INET6 => socket.add_membership(RTNLGRP_IPV6_ROUTE).unwrap(),
+            AF_INET => socket
+                .socket_mut()
+                .add_membership(RTNLGRP_IPV4_ROUTE)
+                .unwrap(),
+            AF_INET6 => socket
+                .socket_mut()
+                .add_membership(RTNLGRP_IPV6_ROUTE)
+                .unwrap(),
             _ => unreachable!(),
         }
 
@@ -409,8 +415,14 @@ impl NetLinkNetInfo {
 
         packet.serialize(&mut buf[..]);
 
-        socket.add_membership(RTNLGRP_IPV4_IFADDR).unwrap();
-        socket.add_membership(RTNLGRP_IPV6_IFADDR).unwrap();
+        socket
+            .socket_mut()
+            .add_membership(RTNLGRP_IPV4_IFADDR)
+            .unwrap();
+        socket
+            .socket_mut()
+            .add_membership(RTNLGRP_IPV6_IFADDR)
+            .unwrap();
 
         if let Err(e) = socket.send(&buf[..]).await {
             log::warn!("SEND ERROR {}", e);
@@ -449,12 +461,10 @@ impl NetLinkNetInfo {
 
     async fn run(sni: SharedNetInfo, chan: tokio::sync::mpsc::Sender<()>) {
         let mut socket = Socket::new(protocols::NETLINK_ROUTE).unwrap();
-        let _port_number = socket.bind_auto().unwrap().port_number();
         let mut seq = 1;
-        socket.connect(&SocketAddr::new(0, 0)).unwrap();
+        socket.socket_mut().connect(&SocketAddr::new(0, 0)).unwrap();
 
-        let mut receive_buffer = vec![0; 4096];
-        let mut offset = 0;
+        let mut bytes = bytes::BytesMut::with_capacity(4096);
 
         NetLinkNetInfo::send_linkdump(&mut socket, &mut seq).await;
         enum State {
@@ -466,10 +476,9 @@ impl NetLinkNetInfo {
         }
         let mut state = State::ReadingLink;
         // we set the NLM_F_DUMP flag so we expect a multipart rx_packet in response.
-        while let Ok(size) = socket.recv(&mut receive_buffer[..]).await {
+        while let Ok(()) = socket.recv(&mut bytes).await {
             loop {
-                let bytes = &receive_buffer[offset..];
-                let rx_packet = <NetlinkMessage<RtnlMessage>>::deserialize(bytes).unwrap();
+                let rx_packet = <NetlinkMessage<RtnlMessage>>::deserialize(&bytes).unwrap();
 
                 if NetLinkNetInfo::process_message(&sni, &rx_packet).await {
                     match state {
@@ -495,12 +504,6 @@ impl NetLinkNetInfo {
                         }
                         State::Done => {}
                     }
-                }
-
-                offset += rx_packet.header.length as usize;
-                if offset == size || rx_packet.header.length == 0 {
-                    offset = 0;
-                    break;
                 }
             }
         }

crates/erbium-net/Cargo.toml
@@ -6,11 +6,12 @@ description = "Network services for small/home networks - Low level networking a
 version.workspace = true
 
 [dependencies]
+bytes = { version = ">=1.3" }
 futures = "0.3.8"
 log = "0.4"
 mio = { version = "0.8", features=["net", "os-poll"] }
 netlink-packet-route = "0.13"
-netlink-sys = { version=">=0.7, <0.8", features=["tokio_socket"] }
+netlink-sys = { version="0.8", features=["tokio_socket"] }
 nix = { version = "0.26", features=["net"] }
 tokio = { version = "1.8.4", features = ["full"] }
 

commit 7d9dfb27eb112465b9f13991caef5949e59d0073

Handle more than one NetlinkMessage received in the same socket buffer.
Without this, all but the first NetlinkMessage in each
socket.recv_from_full() bufferfull is dropped, leading to overlooked
interfaces, IP addresses, and other netlink objects.

This used to be handled correctly but regressed in
commit 15bde7f729d9dbce1a2bd0070ca219958aafd5db

crates/erbium-net/src/netinfo.rs
@@ -465,33 +465,37 @@ impl NetLinkNetInfo {
         let mut state = State::ReadingLink;
         // we set the NLM_F_DUMP flag so we expect a multipart rx_packet in response.
         while let Ok((pkt, _)) = socket.recv_from_full().await {
-            let rx_packet = <NetlinkMessage<RtnlMessage>>::deserialize(&pkt).unwrap();
+            let mut offset = 0;
+            while offset < pkt.len() {
+                let rx_packet = <NetlinkMessage<RtnlMessage>>::deserialize(&pkt[offset..]).unwrap();
+                offset += rx_packet.header.length as usize;
 
-            if NetLinkNetInfo::process_message(&sni, &rx_packet).await {
-                match state {
-                    State::ReadingLink => {
-                        trace!("Finished Link");
-                        NetLinkNetInfo::send_addrdump(&mut socket, &mut seq).await;
-                        state = State::ReadingAddr
+                if NetLinkNetInfo::process_message(&sni, &rx_packet).await {
+                    match state {
+                        State::ReadingLink => {
+                            trace!("Finished Link");
+                            NetLinkNetInfo::send_addrdump(&mut socket, &mut seq).await;
+                            state = State::ReadingAddr
+                        }
+                        State::ReadingAddr => {
+                            trace!("Finished Addr");
+                            NetLinkNetInfo::send_routedump(&mut socket, &mut seq, AF_INET as u8).await;
+                            state = State::ReadingRoute4
+                        }
+                        State::ReadingRoute4 => {
+                            trace!("Finished Route4");
+                            NetLinkNetInfo::send_routedump(&mut socket, &mut seq, AF_INET6 as u8).await;
+                            state = State::ReadingRoute6
+                        }
+                        State::ReadingRoute6 => {
+                            // Try and inform anyone listening that we have completed.
+                            // But if it fails, don't worry, we'll send another one soonish.
+                            trace!("Finished Route6");
+                            let _ = chan.try_send(());
+                            state = State::Done
+                        }
+                        State::Done => {}
                     }
-                    State::ReadingAddr => {
-                        trace!("Finished Addr");
-                        NetLinkNetInfo::send_routedump(&mut socket, &mut seq, AF_INET as u8).await;
-                        state = State::ReadingRoute4
-                    }
-                    State::ReadingRoute4 => {
-                        trace!("Finished Route4");
-                        NetLinkNetInfo::send_routedump(&mut socket, &mut seq, AF_INET6 as u8).await;
-                        state = State::ReadingRoute6
-                    }
-                    State::ReadingRoute6 => {
-                        // Try and inform anyone listening that we have completed.
-                        // But if it fails, don't worry, we'll send another one soonish.
-                        trace!("Finished Route6");
-                        let _ = chan.try_send(());
-                        state = State::Done
-                    }
-                    State::Done => {}
                 }
             }
         }